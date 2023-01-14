ddbfe0de7fe7276bc5ea144d0555b03328180f71
mopizin <121594189+mopizin@users.noreply.github.com>
2023-01-14 20:07:34 -0300

    Add files via upload

--- js/js.js
+++ js/js.js
@@ -1,13 +1,3 @@
-$(function() {
-  $('.h').hover(function() {
-    $('#m').css('opacity', '30%');
-    $('#m').css('transition', 'opacity 1s ease');
-  }, function() {
-    $('#m').css('opacity', '100%');
-  });
-});
-
-
 var myDate = new Date();
 		var h = myDate.getHours();
 
@@ -23,5 +13,15 @@ var myDate = new Date();
 		    greet =  'Boa noite';
 		  }
 		document.getElementById('j').innerHTML = greet + ' e Saudações visitante, como vai?, espero que esteja bem, atualização nova e está bem simples para navegar, então não tenho muito o que falar nesta pequena apresentação mas espero que curta o meu trabalhos já feito, mesmo sendo pra anime ou entre outras categorias! ';
+
+
+$(function() {
+  $('.h').hover(function() {
+    $('#m').css('opacity', '30%');
+    $('#m').css('transition', 'opacity 1s ease');
+  }, function() {
+    $('#m').css('opacity', '100%');
+  });
+});
 
 document.addEventListener("contextmenu", function (e){e.preventDefault();}, false);
